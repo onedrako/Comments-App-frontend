cf00b2bb3e9ba75e6d42b3a6b7324fd62cf2b8e0
onedrako <onedrako@gmail.com>
2022-08-24 00:32:44 -0500

    Add delete options to each comment component

--- src/components/CommentItem.tsx
+++ src/components/CommentItem.tsx
@@ -1,9 +1,19 @@
 import { commentsType } from '@customTypes/commentTypes'
 import React, { useState } from 'react'
+import { CommentsService } from '../service/comments.service'
 import { UpdateComment } from './UpdateComment'
 
+// Compponent that render a comment with the update and delete options
 const CommentItem = ({ comment }: {comment: commentsType}) => {
   const [isEditing, setIsEditing] = useState(false)
+  const [isDeleting, setIsDeleting] = useState(false)
+
+  const service = new CommentsService()
+
+  const handleDelete = async (id: number) => {
+    await service.deleteComment(id)
+    setIsDeleting(false)
+  }
 
   return (
     <div>
@@ -12,11 +22,10 @@ const CommentItem = ({ comment }: {comment: commentsType}) => {
 
     <div>
       {isEditing && <UpdateComment changeState={setIsEditing} commentId={comment.id} />}
-      <button onClick={() => setIsEditing(!isEditing) }>{!isEditing ? 'Edit' : 'Cancel'}</button>
+      {!isDeleting && <button onClick={() => setIsEditing(!isEditing) }>{!isEditing ? 'Edit' : 'Cancel'}</button>}
 
-      {!isEditing && (
-        <button>Delete</button>
-      )}
+      {isDeleting && <button onClick={() => handleDelete(comment.id)} >Confirm Delete</button>}
+      {!isEditing && (<button onClick={() => setIsDeleting(!isDeleting)}>{!isDeleting ? 'Delete' : 'Cancel'}</button>)}
     </div>
 
   </div>
